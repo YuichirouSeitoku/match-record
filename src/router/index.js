7f7d98b961339d93592ae70ac758a72e9cf50aa8
YuichirouSeitoku <49630718+YuichirouSeitoku@users.noreply.github.com>
2019-12-31 15:03:04 +0900

    Revert "#3 one character tag routing"

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import TopMenu from '@/components/TopMenu'
-import SelectCardTop from '../components/SelectCard/SelectCardTop'
-import SelectOneCharacter from '../components/SelectCard/SelectOneCharacter'
-import QuestionExistCard from '../components/Question/QuestionExistCard'
-import QuestionWhichTake from '../components/Question/QuestionWhichTake'
-import QuestionExistOtetsuki from '../components/Question/QuestionExistOtetsuki'
-import QuestionOtetsukiDetail from '../components/Question/QuestionOtetsukiDetail'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -14,38 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'TopMenu',
-      component: TopMenu
-    },
-    {
-      path: '/SelectCardTop',
-      name: 'SeletcCardTop',
-      component: SelectCardTop
-    },
-    {
-      path: '/SelectOneCharacter',
-      name: 'SelectOneCharacter',
-      component: SelectOneCharacter
-    },
-    {
-      path: '/QuestionExistCard',
-      name: 'QuestionExistCard',
-      component: QuestionExistCard
-    },
-    {
-      path: '/QuestionWhichTake',
-      name: 'QuestionWhichTake',
-      component: QuestionWhichTake
-    },
-    {
-      path: '/QuestionExistOtetsuki',
-      name: 'QuestionExistOtetsuki',
-      component: QuestionExistOtetsuki
-    },
-    {
-      path: '/QuestionOtetsukiDetail',
-      name: 'QuestionOtetsukiDetail',
-      component: QuestionOtetsukiDetail
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
