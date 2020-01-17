01b6abfe191a62d57f7659e0a29098cb00b38edc
YuichirouSeitoku <kazeso.kazewo@gmail.com>
2020-01-17 16:46:35 +0900

    [clean]issue8_#20_separate_question_components

---
+++ src/components/Question/QuestionExistOtetsuki.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div>
+            <p>自分 or 相手がお手つきをした？</p>
+        </div>
+        <div class="columns">
+            <div class="column">
+                <RouterLink to ='/QuestionOtetsukiDetail'>
+                    <div class="box">
+                        <p>はい</p>
+                    </div>
+                </RouterLink>
+            </div>
+
+            <div class="column">
+                <div @click="updateOtetsukiUser('なし')">
+                    <RouterLink to = '/'>
+                        <div class="box">
+                            <p>いいえ</p>
+                        </div>
+                    </RouterLink>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'QuestionExistOtetsuki',
+  methods: {
+    updateOtetsukiUser (value) {
+      this.$store.dispatch('updateOtetsukiUser', {otetsuki_user: value})
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router'
 import InputData from '@/components/InputData'
 import TopMenu from '@/components/TopMenu'
 import ShowChart from '@/components/ShowChart'
+import QuestionExistCard from '../components/Question/QuestionExistCard'
+import QuestionWhichTake from '../components/Question/QuestionWhichTake'
+import QuestionOffenseOrDefense from '../components/Question/QuestionOffenseOrDefense'
+import QuestionExistOtetsuki from '../components/Question/QuestionExistOtetsuki'
+import QuestionOtetsukiDetail from '../components/Question/QuestionOtetsukiDetail'
 
 Vue.use(Router)
 
@@ -22,6 +27,31 @@ export default new Router({
       path: '/ShowChart',
       name: 'ShowChart',
       component: ShowChart
+    },
+    {
+      path: '/QuestionExistCard',
+      name: 'QuestionExistCard',
+      component: QuestionExistCard
+    },
+    {
+      path: '/QuestionWhichTake',
+      name: 'QuestionWhichTake',
+      component: QuestionWhichTake
+    },
+    {
+      path: '/QuestionOffenseOrDefense',
+      name: 'QuestionOffenseOrDefense',
+      component: QuestionOffenseOrDefense
+    },
+    {
+      path: '/QuestionExistOtetsuki',
+      name: 'QuestionExistOtetsuki',
+      component: QuestionExistOtetsuki
+    },
+    {
+      path: '/QuestionOtetsukiDetail',
+      name: 'QuestionOtetsukiDetail',
+      component: QuestionOtetsukiDetail
     }
   ]
 })
