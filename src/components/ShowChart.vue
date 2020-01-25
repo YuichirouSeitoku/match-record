a9eb6bcda6120a36fbdbd9b3f846a63f37d871a1
YuichirouSeitoku <kazeso.kazewo@gmail.com>
2020-01-26 03:36:09 +0900

    [style]fix Chart style

--- src/components/ShowChart.vue
+++ src/components/ShowChart.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
-        <DoughnutChart></DoughnutChart>
-        <LineChart></LineChart>
+        <div class="box">
+            <p class="has-text-centered">札取得時に攻めた割合・守った割合</p>
+            <DoughnutChart></DoughnutChart>
+        </div>
+        <div class="box">
+            <p class="has-text-centered">枚数差の推移</p>
+            <LineChart></LineChart>
+        </div>
     </div>
 </template>
 <script>
