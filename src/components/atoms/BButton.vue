24600e97da7edf4f3fee77852a13c5b61cb15233
YuichirouSeitoku <kazeso.kazewo@gmail.com>
2020-05-11 11:18:00 +0900

    [add]#54 BButton.vue

---
+++ src/components/atoms/BButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="clickedBButton">
+    <v-btn>{{ buttonName }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BButton',
+  props: {
+    buttonName: String
+  },
+  methods: {
+    clickedBButton () {
+      this.$emit('clickedBButton')
+    }
+  }
+}
+</script>
